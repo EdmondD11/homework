ff294fa82282d131403d0d7fc56831d736d1dad5
Edmond <jahijaedmond@gmail.com>
2025-04-13 10:40:58 +0200

    Deleted folders

--- Schema/chef.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ChefSchema = new mongoose.Schema({
-  name: String,
-  experience: String,
-  specialty: String,
-});
-
-export default mongoose.model("Chef", ChefSchema);
